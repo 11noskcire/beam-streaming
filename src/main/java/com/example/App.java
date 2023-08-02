cf96ee7bf3fb76ac82971a7e56742947bd94ee2f
erickson <erickson@skinmystery.com>
2023-08-02 19:40:13 +0000

    use Schema

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -20,19 +20,18 @@ import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.options.StreamingOptions;
+import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.transforms.Filter;
+import org.apache.beam.sdk.transforms.JsonToRow;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.SimpleFunction;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionTuple;
+import org.apache.beam.sdk.values.Row;
 import org.apache.beam.sdk.values.TupleTag;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.joda.time.Instant;
-
-import com.example.model.Transaction;
-import com.example.model.TransactionDetail;
-import com.example.utility.Gson;
 
 public class App {
     public interface Options extends StreamingOptions {
@@ -55,32 +54,21 @@ public class App {
                         .withTopics(List.of("transactions", "transaction-details"))
                         .withKeyDeserializer(StringDeserializer.class)
                         .withValueDeserializer(StringDeserializer.class));
-        PCollection<Transaction> transactions = kafkaRecords
-                .apply("FilterTopic", Filter.by(kafkaRecord -> kafkaRecord.getTopic().equals("transactions")))
-                .apply("DeserializeJson", MapElements
-                        .via(new SimpleFunction<KafkaRecord<String, String>, Transaction>() {
-                            @Override
-                            public Transaction apply(KafkaRecord<String, String> kafkaRecord) {
-                                Gson gson = new Gson();
-                                String json = kafkaRecord.getKV().getValue();
-                                Transaction t = gson.fromJson(json, Transaction.class);
-                                t.process_date = new Instant(kafkaRecord.getTimestamp());
-                                return t;
-                            }
-                        }));
-        PCollection<TransactionDetail> transactionDetails = kafkaRecords
-                .apply("FilterTopic", Filter.by(kafkaRecord -> kafkaRecord.getTopic().equals("transaction-details")))
-                .apply("DeserializeJson", MapElements
-                        .via(new SimpleFunction<KafkaRecord<String, String>, TransactionDetail>() {
-                            @Override
-                            public TransactionDetail apply(KafkaRecord<String, String> kafkaRecord) {
-                                Gson gson = new Gson();
-                                String json = kafkaRecord.getKV().getValue();
-                                TransactionDetail td = gson.fromJson(json, TransactionDetail.class);
-                                td.process_date = new Instant(kafkaRecord.getTimestamp());
-                                return td;
-                            }
-                        }));
+        var transactions = deserializeKafkaJson(
+                kafkaRecords,
+                "transactions",
+                Schema.builder()
+                        .addStringField("trx_id")
+                        .addStringField("user_id")
+                        .addDateTimeField("created_date"));
+        var transactionDetails = deserializeKafkaJson(
+                kafkaRecords,
+                "transaction-details",
+                Schema.builder()
+                        .addStringField("trx_id")
+                        .addStringField("product_id")
+                        .addInt64Field("qty")
+                        .addDecimalField("price"));
 
         PCollectionTuple
                 .of(new TupleTag<>("TRX"), transactions)
@@ -90,7 +78,7 @@ public class App {
                             SELECT *
                             FROM HOP(
                                 (SELECT * FROM TRX),
-                                DESCRIPTOR(PROCESS_DATE),
+                                DESCRIPTOR(EVENT_TIMESTAMP),
                                 "INTERVAL 5 SECOND",
                                 "INTERVAL 1 MINUTE"
                             )
@@ -100,7 +88,7 @@ public class App {
                                 QTY*PRICE AS TOTAL
                             FROM HOP(
                                 (SELECT * FROM TRX_DETAILS),
-                                DESCRIPTOR(PROCESS_DATE),
+                                DESCRIPTOR(EVENT_TIMESTAMP),
                                 "INTERVAL 5 SECOND",
                                 "INTERVAL 1 MINUTE"
                             )
@@ -131,4 +119,23 @@ public class App {
         pipeline.run().waitUntilFinish();
     }
 
+    private static <K> PCollection<Row> deserializeKafkaJson(
+            PCollection<KafkaRecord<K, String>> pCollection,
+            String topic,
+            Schema.Builder schemaBuilder) {
+        var schema = schemaBuilder.addDateTimeField("event_timestamp").build();
+        return pCollection
+                .apply(Filter.by(k -> k.getTopic().equals(topic)))
+                .apply(MapElements.via(new SimpleFunction<KafkaRecord<?, String>, String>() {
+                    @Override
+                    public String apply(KafkaRecord<?, String> k) {
+                        String json = k.getKV().getValue().trim();
+                        json = String.format("%s, \"event_timestamp\": \"%s\"}",
+                                json.substring(0, json.length() - 1),
+                                new Instant(k.getTimestamp()).toString());
+                        return json;
+                    }
+                }))
+                .apply(JsonToRow.withSchema(schema));
+    }
 }
